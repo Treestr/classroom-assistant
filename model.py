e285d5d4bb9841508ae23810fc23de4ed915bdf2
Ursula Conway <uconway22@gmail.com>
2024-04-15 16:21:53 -0700

    model.py edits

--- model.py
+++ model.py
@@ -6,37 +6,50 @@ class Teacher(db.Model):
     __tablename__ = "teachers"
 
     teacher_id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
-#add class: fname, lname (or full name???), teacher_email
+    fname = db.Column(db.String(20), nullable = False, unique = True)
+    lname - db.Column(db.String(20), nullable = False, unique = True)
+    teacher_email = db.Column(db.String(50), nullable = False, unique = True)
+    #foreign key relationships
 
 class Student(db.Model):
 
     __tablename__ = "students"
 
     student_id = db.Column(db.Integer, primary_key=True, autoincrment=True) 
-    #add class: fname, lname (or name??), icon, qr/barcode, flag (students not allowed out at same time)
+    fname = db.Column(db.String(20), nullable=False, unique=True)
+    lname = db.Column(db.String(20), nullable = False, unique = True)
+    #icon, qr/barcode, flags (not allowed same time)
+    #foreign key relationships
+   
 
 ##ADD CONNECTOR CLASS: STUDENT/HOMEROOM
 
-class Location(db.Model):
+class Location(db.Model):                       
 
     __tablename__ = "locations"
 
     student_locations_id = db.Column(db.Integer, primary_key=True,)
-##add class: location_name
+    location_name = db.Column(db.String(20), nullable=False, unique=True)
+
 
 class HomeRoom(db.Model):#Homeroom = "class" py/school context naming challenge
     
     __tablename__ = "homeroom"
     
     homeroom_id = db.Column(db.Integer, primary_key=True, autoincrement=True,)
-    #homeroom_name, teacher_id FK, student_id FK
+    homeroom_name = db.Column(db.String), nullable=False
+    # teacher_id = db.relationship('Teacher', ADDDDDDD (back-populates?))
+    # student_id = db.relationship                                                  
+
 
 class Group(db.Model):
 
     __tablename__ = "groups"
 
     group_id = db.Column(db.Integer, primary_key=True, autoincrement=True,)#is comma correct check
-    #add: group_name, class_id FK, ((assignment_id????))
+    group_name = db.Column(db.String(20), unique=True)
+    #FK class_id
+    
 
     #membership_record is a CONNECTOR table
 
@@ -52,6 +65,10 @@ class Assignment(db.Model):
     __tablename__ = "assignments"
 
     assignment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    assignment_title = db.Column(db.String(20)
+    assignment_desc = db.Column(db.String(100))
+    #due_date
+    #FK teacher_id, student_id
     #add columns: title, description, due_date, teacher_id FK
 
 ##CREATE A CLASSS: CONNECTOR FOR STUDENTS/ASSIGNMENTS
@@ -60,7 +77,10 @@ class Leave_Record(db.Model):
     __tablename__ = "leave_records"
 
     leave_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    ##remaining columns: student_id FK, leave_start_time, leave_end_time, location_id Fk
+    leave_time = 
+    return_time = 
+    #FK: location_id, student_id
+    
 
 class Location(db.Model):
     
