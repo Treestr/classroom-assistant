e655ef20d14f1d4f57aa7b870fb60bfb80adb0e5
Ursula Conway <uconway22@gmail.com>
2024-05-11 16:41:35 -0700

    create image file

--- static/ajax.js
+++ static/ajax.js
@@ -1,2 +1,12 @@
-alert("we good?");
-query selector All to select all CanvasCaptureMediaStreamTrack, 
+
+const statusLinks =document.querySelectorAll('a.status-link');
+for (const statusLink of statusLinks) {
+    statusLink.addEventListener('click', (evt) => {
+        evt.preventDefault();
+
+        const cardId = evt.target.getAttribute('data-card-id');
+        const card = document.getElementById(cardId);
+
+        card.classList.toggle('pink-background');
+    })
+}
